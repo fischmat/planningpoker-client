742bc6c2b9e63ec32f7246561d3ae7317605458a
Matthias Fisch <mail@matthias-fisch.de>
2023-05-17 21:01:00 +0200

    Fix cookies not sent in XHR

--- src/services/API.ts
+++ src/services/API.ts
@@ -1,8 +1,15 @@
-import axios from 'axios'
+import axios from "axios";
+import { wrapper } from "axios-cookiejar-support";
+import { CookieJar } from "tough-cookie";
 
 export const apiBaseUrl =
   process.env.NODE_ENV === "development" ? "http://localhost:8080/api" : "/api";
 
-export const api = axios.create({
-  baseURL: apiBaseUrl
-});
+export const jar = new CookieJar();
+export const api = wrapper(
+  axios.create({
+    baseURL: apiBaseUrl,
+    jar: jar,
+    withCredentials: true
+  })
+);
