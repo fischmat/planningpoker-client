671ef9900eb2eb758998671b26108368bdac07a6
Matthias Fisch <mail@matthias-fisch.de>
2023-06-01 20:31:39 +0200

    Escape environment variable

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 echo "Setting $BASE_PATH as API base path..."
-sed -i "s/###BASE_PATH###/${BASE_PATH}/g" /usr/share/nginx/html/assets/*
+ESCAPED_BASE_PATH=$(echo "$BASE_PATH" | sed 's#/#\\/#g')
+sed -i "s/###BASE_PATH###/$ESCAPED_BASE_PATH/g" /usr/share/nginx/html/assets/*
 
 echo "Starting nginx..."
 nginx -g "daemon off;"
